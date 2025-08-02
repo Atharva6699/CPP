750668fc36540aa77d9f26dea7097f795fc71162
Atharva6699 <atharvagawande0609@gmail.com>
2025-08-02 14:20:07 +0530

    .

--- ReverseTrianglechar.cpp
+++ ReverseTrianglechar.cpp
@@ -1,19 +1,18 @@
-#include <iostream>                
+#include <iostream>
 using namespace std;
-int main() {
 
+int main() {
     int n = 5;
-    char ch = 'A';
-    
-    for(int i=0; i < n; i++) {
-        char ch = 'A';
-        
-        for(int j=i + 1; j > 0; j++) {
-            
+
+    for(int i = 0; i < n; i++) {
+        char ch = 'A' + i;
+
+        for(int j = 0; j <= i; j++) {
             cout << ch << " ";
-            ch --;
+            ch--;
         }
         cout << endl;
     }
+
     return 0;
 }
